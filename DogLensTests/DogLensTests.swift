070d4f9b9084f59d5736540d57ad2a46ba72578e
BryanSamuelIan <146687755+BryanSamuelIan@users.noreply.github.com>
2026-08-21 14:59:36 +0700

    Initial commit

---
+++ DogLensTests/DogLensTests.swift
@@ -0,0 +1,19 @@
+//
+//  DogLensTests.swift
+//  DogLensTests
+//
+//  Created by Bryan Samuel on 21/08/26.
+//
+
+import Testing
+@testable import DogLens
+
+struct DogLensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
